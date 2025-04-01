f933e311e82a48a048eb3e53eac6ceeec80440b3
SlimDReaper <ronnyduran35@gmail.com>
2025-04-01 19:43:18 -0400

    Creación ApiResponse

---
+++ EasyBookingApp/EasyBooking.Frontend/Models/ApiResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace EasyBooking.Frontend.Models
+{
+    public class ApiResponse<T>
+    {
+        public string Message { get; set; } = string.Empty;
+        public T? Data { get; set; }
+        public bool Success { get; set; }
+        public string? Error { get; set; }
+    }
+}
