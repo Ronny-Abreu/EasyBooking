713245391e33e054fd68694bfd17cbacdf61bcea
SlimDReaper <ronnyduran35@gmail.com>
2025-04-01 19:25:07 -0400

    Creación contracts para usuario "IUsuarioService"

---
+++ EasyBookingApp/EasyBooking.Application/Contracts/IUsuarioService.cs
@@ -0,0 +1,15 @@
+﻿using EasyBooking.Domain.Entities;
+using System.Threading.Tasks;
+
+namespace EasyBooking.Application.Contracts
+{
+    public interface IUsuarioService
+    {
+        Task<Usuario> ObtenerUsuarioPorIdAsync(int id);
+        Task<IEnumerable<Usuario>> ObtenerUsuariosAsync();
+        Task<Usuario> CrearUsuarioAsync(Usuario usuario);
+        Task<Usuario> ActualizarUsuarioAsync(Usuario usuario);
+        Task EliminarUsuarioAsync(int id);
+        Task<Usuario> ObtenerPorEmailAsync(string email);
+    }
+}
