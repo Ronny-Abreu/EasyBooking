d6689cc3b82ee94b0a93360ca7f95e0c19c3280f
SlimDReaper <ronnyduran35@gmail.com>
2025-04-01 20:13:47 -0400

    Actualización de servicios para program front

--- EasyBookingApp/EasyBooking.Frontend/Program.cs
+++ EasyBookingApp/EasyBooking.Frontend/Program.cs
@@ -1,23 +1,35 @@
+using EasyBooking.Frontend.Services;
+using EasyBooking.Frontend.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Configuración de servicios ANTES de builder.Build()
+builder.Services.AddHttpClient(); // HttpClient
+builder.Services.AddScoped<HttpClientService>(); // Servicios personalizados
+builder.Services.AddScoped<UsuarioClientService>();
+builder.Services.Configure<ApiSettings>(builder.Configuration.GetSection("ApiSettings")); // Configuración
+
+// Otros servicios
 builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
+var app = builder.Build(); // A partir de aquí, NO se pueden registrar más servicios
 
-// Configure the HTTP request pipeline.
+// Configuración del middleware (esto sí va después de Build())
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
+
+
+
+
+
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
