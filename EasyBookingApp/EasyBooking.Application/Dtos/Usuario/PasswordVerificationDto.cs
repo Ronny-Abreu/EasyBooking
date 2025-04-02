c719ad338ae064ccc0c57d66e3d63c06ee0cb181
SlimDReaper <ronnyduran35@gmail.com>
2025-04-01 20:08:51 -0400

    Creación para la verificación de contraseña

---
+++ EasyBookingApp/EasyBooking.Application/Dtos/Usuario/PasswordVerificationDto.cs
@@ -0,0 +1,9 @@
+﻿namespace EasyBooking.Application.Dtos
+{
+    public class PasswordVerificationDto
+    {
+        public int UserId { get; set; }
+        public string Password { get; set; }
+    }
+
+}
