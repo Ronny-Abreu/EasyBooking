02e58ab1fbd9f4542a5685d4fb384693ae8236d1
SlimDReaper <ronnyduran35@gmail.com>
2025-04-01 20:09:25 -0400

    Creación Dto del UpdateProfile

---
+++ EasyBookingApp/EasyBooking.Application/Dtos/Usuario/UpdateProfileDto.cs
@@ -0,0 +1,15 @@
+﻿namespace EasyBooking.Application.Dtos
+{
+    public class UpdateProfileDto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Telefono { get; set; }
+        public bool IsEmailVerified { get; set; }
+        public string Password { get; set; }
+        public string CurrentPassword { get; set; }
+    }
+}
