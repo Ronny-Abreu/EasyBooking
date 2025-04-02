106ba33f13a04a5f061a6bc499e76b0eb46abfad
SlimDReaper <ronnyduran35@gmail.com>
2025-04-01 20:06:05 -0400

    Configuración Program del Api

--- EasyBookingApp/EasyBooking.Api/Program.cs
+++ EasyBookingApp/EasyBooking.Api/Program.cs
@@ -1,11 +1,48 @@
+using Microsoft.EntityFrameworkCore;
+using EasyBooking.Application.Contracts;
+using EasyBooking.Application.Services;
+using EasyBooking.Persistence.Interfaces;
+using EasyBooking.Persistence.Context;
+using EasyBooking.Persistence.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+//Configuracion Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configurar la base de datos
+builder.Services.AddDbContext<EasyBookingDbContext>(options =>
+  options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+// Registrar Servicios
+builder.Services.AddScoped<IUsuarioService, UsuarioService>();  // Servicio de usuario
+
+
+// Registrar repositorios
+builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+
+// Registrar el servicio de correo electrónico
+builder.Services.AddScoped<IEmailService, EmailService>();
+
+
+
+
+
+// Configurar CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
@@ -17,9 +54,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors("AllowAll");
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
