b2d2d95c7b246776e204d006938f50482fe58fbc
SlimDReaper <ronnyduran35@gmail.com>
2025-04-01 20:10:01 -0400

    Creación Dto del envío de codigo para resetear contraseña

---
+++ EasyBookingApp/EasyBooking.Application/Dtos/Usuario/VerifyResetCodeDto.cs
@@ -0,0 +1,10 @@
+﻿
+namespace EasyBooking.Application.Dtos
+{
+    public class VerifyResetCodeDto
+    {
+        public string Email { get; set; }
+        public string Code { get; set; }
+    }
+
+}
