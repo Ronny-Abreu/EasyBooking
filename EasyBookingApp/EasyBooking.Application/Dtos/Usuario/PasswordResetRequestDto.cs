f21128038a61b4a6f8911732c85ad0632325a458
SlimDReaper <ronnyduran35@gmail.com>
2025-04-06 01:12:01 -0400

    atributo agregado al Dto de PasswordResetRquestDto

--- EasyBookingApp/EasyBooking.Application/Dtos/Usuario/PasswordResetRequestDto.cs
+++ EasyBookingApp/EasyBooking.Application/Dtos/Usuario/PasswordResetRequestDto.cs
@@ -4,6 +4,7 @@ namespace EasyBooking.Application.Dtos
     public class PasswordResetRequestDto
     {
         public string Email { get; set; }
+        public int? UserId { get; set; }
     }
 
 }
