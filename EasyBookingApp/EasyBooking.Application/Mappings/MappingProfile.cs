6c471ee15c0c250ad230b94b43420ca1ef98e0dd
tuerre <ronnyduran35@gmail.com>
2025-04-17 17:51:42 -0400

    New packageReference

---
+++ EasyBookingApp/EasyBooking.Application/Mappings/MappingProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using EasyBooking.Application.Dtos;
+using EasyBooking.Domain.Entities;
+
+namespace EasyBooking.Application.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Usuario mappings
+            CreateMap<Usuario, UsuarioDto>();
+            CreateMap<RegistroUsuarioDto, Usuario>();
+
+            // Hotel mappings
+            CreateMap<Hotel, HotelDto>()
+             .ForMember(dest => dest.Servicios, opt => opt.MapFrom(src => src.Servicios));
+
+            // Reserva mappings
+            CreateMap<Reserva, ReservaDto>()
+                .ForMember(dest => dest.Estado, opt => opt.MapFrom(src => src.Estado.ToString()));
+            CreateMap<CrearReservaDto, Reserva>();
+        }
+    }
+}
