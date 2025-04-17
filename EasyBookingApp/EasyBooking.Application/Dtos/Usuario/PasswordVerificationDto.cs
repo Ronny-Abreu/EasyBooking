ab11546635e38fcf079fafb40e18a5ddebed7bbb
tuerre <ronnyduran35@gmail.com>
2025-04-17 17:35:24 -0400

    Delete: Dtos olds innecesaries
    The Data has been replaced by new Data

--- EasyBookingApp/EasyBooking.Application/Dtos/Usuario/PasswordVerificationDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EasyBooking.Application.Dtos
-{
-    public class PasswordVerificationDto
-    {
-        public int UserId { get; set; }
-        public string Password { get; set; }
-    }
-
-}
